e753af98f30c68bc3c4c5eac5e894d1125ac87e2
Jorge Flores <jorge.flores@Jorges-MacBook-Pro.local>
2017-09-26 13:19:53 -0300

    Last cahnges

--- go_connect.go
+++ go_connect.go
@@ -67,6 +67,7 @@ func downloadFromUrl(url string) {
 	fmt.Println(n, "bytes downloaded.")
 }
 
+// Check if string is numeric
 func IsNumeric(s string) bool {
   _, err := strconv.ParseFloat(s, 64)
   return err == nil
@@ -85,58 +86,69 @@ func main() {
   data, err := ioutil.ReadFile("allnodes")
   check(err)
 
-  v := Project{}
-  err = xml.Unmarshal([]byte(data), &v)
+  data_unmarsh := Project{}
+  err = xml.Unmarshal([]byte(data), &data_unmarsh)
   check(err)
 
+  if len(args) < 3 {
+    fmt.Println("Help ")
+    os.Exit(0)
+  }
+
+  // Covert arg[1] to Title (first char uper case) to match Node struct
+  args[1] = strings.Title(args[1])
+
   // Get and sort xml values by query arguments
-  m := make(map[string]string)
+  data_array := make(map[string]string)
   keys := []string{}
   
-  for  _, value := range v.Nodes {
+  for  _, value := range data_unmarsh.Nodes {
     // reflect used to use method dinamicaly
     s := reflect.ValueOf(value)
     f := s.FieldByName(args[1]).Interface().(string)
 
     // populate map and keys if string found
     if strings.Contains(f, args[2]){
-      m[value.Desc] = value.Ip
+      data_array[value.Desc] = value.Ip
       keys = append(keys, value.Desc)  
     }
   }
-  // Sorte de keys 
+  // Sort keys 
   sort.Strings(keys)
-  
-  
-  // Run flux based on option
+   
+  // Initializa flux variables
   count := 1
   cssh_string := ""
   
+  // Run flux based on option
   for _, val := range keys {
+    // No index in arguments - print list
     if len(args) < 4 {
-      fmt.Println(count, " - ", val," :: ", m[val])
+      fmt.Println(count, " - ", val," :: ", data_array[val])
       
     } else { 
-      
+      // Numeric argument 
       if IsNumeric(args[3]) { 
         index, err := strconv.Atoi(args[3])
         check(err)
+        // If match connect and exit
         if index == count {
-          
-          connect.SshConn(m[val])
-          //fmt.Println(count, " - ", val," :: ", m[val])
+          connect.SshConn(data_array[val])
           os.Exit(0)
         }
+      // Literal - connect all list 
       } else if args[3] == "all" {
-        fmt.Println(count, " - ", val," :: ", m[val])
+        connect.SshConn(data_array[val])
+      
+      // Literal - create cssh strig
       } else if args[3] == "cssh" {
-        cssh_string += " " + val + "@" + m[val]
+        cssh_string += " " + val + "@" + data_array[val]
       }
     }
     count++
   }
+  // Run cssh is exist and list is not empty
   if len(cssh_string) > 0 {
     fmt.Println("cssh " + cssh_string)
   }
-
 }
--- libs/connect_darwin.go
+++ libs/connect_darwin.go
@@ -3,10 +3,31 @@
 
 package connect
 
-import "fmt"
+import (
+	"strings"
+	"os/exec"
+)
+
+func wrapInQuotes(text string) string {
+	return "\"" + text + "\""
+}
 
 func SshConn(conn string) {
+	
 
-	fmt.Println("ssh mac: ", conn)
+	ssh_command := "ssh -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=30 -o StrictHostKeyChecking=no "+conn
+	exec_cmd := "create tab with default profile command " + wrapInQuotes(ssh_command)
+	application := wrapInQuotes("iTerm2")
+
+	script := []string{"tell application", application, "\n","tell current window","\n", exec_cmd, "\n", "end tell", "\n", "end tell"}
+	
+	command := strings.Join(script, " ")
+	out := exec.Command("osascript", "-e", command)
+	output, err := out.CombinedOutput()
+
+	_ = output 
+	_ = err
+	//fmt.Println(output)
+	//fmt.Println(err)
 	
 }
