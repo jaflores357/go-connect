b537a23a5b3baf1c2cd811b114e6703c3a11b56e
Jorge Flores <jorge.flores@Jorges-MacBook-Pro.local>
2017-09-27 15:18:24 -0300

    Added support to Terminal mac App

--- libs/connect_darwin.go
+++ libs/connect_darwin.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"os/exec"
 	"os"
+	"fmt"
 )
 
 func Username() string {
@@ -20,12 +21,31 @@ func wrapInQuotes(text string) string {
 func SshConn(conn string) {
 	
 	ssh_command := "ssh -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=30 -o StrictHostKeyChecking=no "+conn
-	exec_cmd := "create tab with default profile command " + wrapInQuotes(ssh_command)
-	application := wrapInQuotes("iTerm2")
+	terminal_app := os.Getenv("TERM_PROGRAM")
+	command := ""
 
-	script := []string{"tell application", application, "\n","tell current window","\n", exec_cmd, "\n", "end tell", "\n", "end tell"}
+	if terminal_app == "iTerm.app" {
 	
-	command := strings.Join(script, " ")
+		exec_cmd := "create tab with default profile command " + wrapInQuotes(ssh_command)
+		application := wrapInQuotes("iTerm2")
+		script := []string{"tell application", application, "\n","tell current window","\n", exec_cmd, "\n", "end tell", "\n", "end tell"}
+		command = strings.Join(script, " ")
+
+	} else if terminal_app == "Apple_Terminal" {
+
+		exec_cmd := "do script " + wrapInQuotes(ssh_command)
+		application := wrapInQuotes("Terminal")
+		script := []string{"tell application ", application, "\n", "tell application \"System Events\" to keystroke \"t\" using {command down}", "\n", exec_cmd, " in front window", "\n","end tell"}
+		command = strings.Join(script, " ")
+		
+	} else {
+
+		fmt.Println("Teminal not supported (yet): "+terminal_app)
+		os.Exit(0)		
+
+	}
+
+
 	out := exec.Command("osascript", "-e", command)
 	output, err := out.CombinedOutput()
 
