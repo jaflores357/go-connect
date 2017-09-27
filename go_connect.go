2d632190e94984670fc151a14e8c463c923fd4dc
Jorge Flores <jorge.flores@Jorges-MacBook-Pro.local>
2017-09-27 10:13:00 -0300

    Asyn download file

--- go_connect.go
+++ go_connect.go
@@ -18,6 +18,8 @@ import (
   "github.com/spf13/viper"
   "syscall"
   "time"
+  "runtime"
+  "sync"
 )  
 
 ///////////////////////////////////////////////////
@@ -46,6 +48,12 @@ func check(e error) {
   }
 }
 
+func logError(e error) {
+  if e != nil {
+    log.Println(e.Error())
+  }
+}
+
 ///////////////////////////////////////////////////
 // Check DB file age
 func checkDBFileAge() bool{
@@ -61,27 +69,52 @@ func checkDBFileAge() bool{
 
 ///////////////////////////////////////////////////
 // Download DB
-func downloadData() {
+func downloadData(wg *sync.WaitGroup) {
   
+  defer wg.Done()
+
   url := viper.GetString("db.url")
   fileName := viper.GetString("db.path")
-	fmt.Println("Downloading", url, "to", fileName)
+  tmp_fileName := fileName + ".tmp"
+	
 
-	// TODO: check file existence first with io.IsExist
-	output, err := os.Create(fileName)
-  check(err)
+  _, err := os.Stat(tmp_fileName)
+  if err != nil {
+    log.Println("Downloading", url, "to", tmp_fileName)
 
-	defer output.Close()
+    var netClient = &http.Client{
+      Timeout: time.Second * viper.GetDuration("db.url_timeout"),
+    }
+    
+    response, err := netClient.Get(url)
+    if err == nil {
+      
+      defer response.Body.Close()
+      output, err := os.Create(tmp_fileName)
+      
+      if err == nil {
 
-	response, err := http.Get(url)
-  check(err)
+        defer output.Close()
+        n, err := io.Copy(output, response.Body)
+        check(err)
+      
+        log.Println(n, "bytes downloaded.")
+        
+        err = os.Rename(tmp_fileName, fileName)
+        check(err)
 
-	defer response.Body.Close()
+      } else {
+        log.Println("here")
+        log.Println(err.Error())  
+      }
+    } else {
+      log.Println(err.Error())
+    }
 
-	n, err := io.Copy(output, response.Body)
-  check(err)
-
-	fmt.Println(n, "bytes downloaded.")
+  } else {
+    log.Println("Skip, already downloading!")
+  }
+  
 }
 
 ///////////////////////////////////////////////////
@@ -94,99 +127,108 @@ func IsNumeric(s string) bool {
 ///////////////////////////////////////////////////
 // Main block
 func main() {
-
-  // Define config file
+  runtime.GOMAXPROCS(1)
+  var wg sync.WaitGroup
+// Config file name and path
   viper.SetConfigName("config")
   viper.AddConfigPath(".")
   
-  // Find and read the config file
+// Find and read the config file
   err := viper.ReadInConfig() 
   check(err)
 
-  // Get arguments
+// Get arguments
   args := os.Args
   
-  // Read xml file
+// Read xml file
   data, err := ioutil.ReadFile(viper.GetString("db.path"))
   if err != nil {
-    fmt.Println("Cant read file: " + viper.GetString("db.path"))
-    downloadData()
-    os.Exit(0)
+    log.Println("Cant read file: " + viper.GetString("db.path"))
+    wg.Add(1)
+    downloadData(&wg)
   }
 
-  // Force DB download or print help when wrong parameters  
+// Force DB download or print help when wrong parameters  
   if len(args) < 3 {
     if len(args) == 2 && args[1] == "download" {
-      downloadData()
+      wg.Add(1)
+      downloadData(&wg)
     } else {
       fmt.Println("Help ")
     }
     os.Exit(0)
   }
 
-  // Force download if DB reach max_file age 
+// Force download if DB reach max_file age 
   if(checkDBFileAge()){
-    downloadData()
+    wg.Add(1)
+    go downloadData(&wg)
   }
 
   data_unmarsh := Project{}
   err = xml.Unmarshal([]byte(data), &data_unmarsh)
   check(err)
 
-  // Covert arg[1] to Title (first char uper case) to match Node struct
+// Covert arg[1] to Title (first char uper case) to match Node struct
   args[1] = strings.Title(args[1])
 
-  // Get and sort xml values by query arguments
+// Get and sort xml values by query arguments
   data_array := make(map[string]string)
   keys := []string{}
   
   for  _, value := range data_unmarsh.Nodes {
-    // reflect used to use method dinamicaly
+// reflect used to use method dinamicaly
     s := reflect.ValueOf(value)
     f := s.FieldByName(args[1]).Interface().(string)
 
-    // populate map and keys if string found
+// populate map and keys if string found
     if strings.Contains(f, args[2]){
       data_array[value.Desc] = value.Ip
       keys = append(keys, value.Desc)  
     }
   }
-  // Sort keys 
+// Sort keys 
   sort.Strings(keys)
    
-  // Initializa flux variables
+// Initializa flux variables
   count := 1
   cssh_string := ""
   
-  // Run flux based on option
+// Run flux based on option
   for _, val := range keys {
-    // No index in arguments - print list
+// No index in arguments - print list
     if len(args) < 4 {
       fmt.Println(count, " - ", val," :: ", data_array[val])
       
     } else { 
-      // Numeric argument 
+// Numeric argument 
       if IsNumeric(args[3]) { 
         index, err := strconv.Atoi(args[3])
         check(err)
-        // If match connect and exit
+// If match, connect and exit
         if index == count {
           connect.SshConn(data_array[val])
           os.Exit(0)
         }
-      // Literal - connect all list 
+// Literal - connect all list 
       } else if args[3] == "all" {
         connect.SshConn(data_array[val])
       
-      // Literal - create cssh strig
+// Literal - create cssh string
       } else if args[3] == "cssh" {
         cssh_string += connect.Username() + "@" + data_array[val] + " "
       }
     }
     count++
   }
-  // Run cssh is exist and list is not empty
+// Run cssh is exist and list is not empty
   if len(cssh_string) > 0 {
-    fmt.Println("cssh " + cssh_string)
+    if (viper.GetBool("cssh.enable")){ 
+      fmt.Println("cssh " + cssh_string)
+    } else {
+      fmt.Println("cssh disabled!")
+    }
+    
   }
+  wg.Wait()
 }
